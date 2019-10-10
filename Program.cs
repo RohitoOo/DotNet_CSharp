23ef66c56c75650f60c33430fbcc481c6bb70306
Rohit Bhambhani <rohito@Rohitos-MacBook-Pro-Work.local>
2019-10-09 22:43:34 -0400

    Added Guessing Game Logic

--- Program.cs
+++ Program.cs
@@ -4,31 +4,96 @@
 namespace CSharp
 {
 
-// Main Class
+    // Main Class
     class Program
     {
         // Entry Point Method
         static void Main(string[] args)
         {
             // Set App variables 
-            string name  = "Rohito Bhambhani";
+            string name = "Rohito Bhambhani";
 
             // Change Text Color
             Console.ForegroundColor = ConsoleColor.Green;
 
             // Write out App Variales 
-            Console.WriteLine("Hello World! Welcome, Mr. " + name );
+            Console.WriteLine("Hello World! Welcome, Mr. " + name);
 
             // Reset Color 
             Console.ResetColor();
 
             // Ask User's name 
-            Console.WriteLine("What is your name ? "); 
-           
-            // Get User's input
-            string input =  Console.ReadLine();
+            Console.WriteLine("What is your name ? ");
 
-            Console.WriteLine("Let's Play a game, {0} " , input);
+            // Get User's input
+            string inputName = Console.ReadLine();
+
+            // Write out User's input 
+            Console.WriteLine("Let's Play a game, {0} ", inputName);
+
+
+            // Set Correct Number To Be Guessed By User
+
+            int correctNumber = 12;
+
+            // Initialize guess variable 
+            int guess = 0;
+
+
+            // Ask user for number 
+            Console.WriteLine("Guess a number between 1 and 15");
+
+            // While Loop Untill The User Guesses the Correct Number 
+            while (guess != correctNumber)
+            {
+                string input = Console.ReadLine();
+
+                // Cast to int and make as value for guess variable 
+
+                guess = Int32.Parse(input);
+
+
+                // Match guess to correctNumber 
+
+                if (guess <= 0 || guess >= 16 )
+                {
+
+                    // Change Text Color
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                    // Write out App Variales 
+                    Console.WriteLine(guess + " is the not between 1 and 15 , " + inputName + ". You dummy!");
+
+                    // Reset Color 
+                    Console.ResetColor();
+                }
+
+                else if (guess != correctNumber)
+                {
+
+                    // Change Text Color
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Write out App Variales 
+                    Console.WriteLine(guess + " is the wrong Number, " + inputName);
+
+                    // Reset Color 
+                    Console.ResetColor();
+
+                }
+                else
+                {
+                    // Change Text Color
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    // Write out App Variales 
+                    Console.WriteLine(guess + " is the CORRECT Number, " + inputName);
+
+                    // Reset Color 
+                    Console.ResetColor();
+                }
+
+            }
         }
     }
 }
