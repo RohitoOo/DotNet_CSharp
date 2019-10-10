32d0e49b1457b0dc473af919a759fa168b0b930e
Rohit Bhambhani <rohito@Rohitos-MacBook-Pro-Work.local>
2019-10-09 22:26:51 -0400

    Added Input Variable

--- Program.cs
+++ Program.cs
@@ -14,13 +14,21 @@ namespace CSharp
             string name  = "Rohito Bhambhani";
 
             // Change Text Color
-
             Console.ForegroundColor = ConsoleColor.Green;
 
             // Write out App Variales 
             Console.WriteLine("Hello World! Welcome, Mr. " + name );
 
+            // Reset Color 
             Console.ResetColor();
+
+            // Ask User's name 
+            Console.WriteLine("What is your name ? "); 
+           
+            // Get User's input
+            string input =  Console.ReadLine();
+
+            Console.WriteLine("Let's Play a game, {0} " , input);
         }
     }
 }
