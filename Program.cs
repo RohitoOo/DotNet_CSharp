8f4a7105dd0aeadb3e31bea43088bc1f52fed7f6
Rohit Bhambhani <rohito@Rohitos-MacBook-Pro-Work.local>
2019-10-09 22:18:33 -0400

    Change text color

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 
+// Namespace 
 namespace CSharp
 {
+
+// Main Class
     class Program
     {
+        // Entry Point Method
         static void Main(string[] args)
         {
+            // Set App variables 
             string name  = "Rohito Bhambhani";
+
+            // Change Text Color
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Write out App Variales 
             Console.WriteLine("Hello World! Welcome, Mr. " + name );
+
+            Console.ResetColor();
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 csc Program.cs ( Compile Program )
 
 mono Program.exe ( Cross Platform Dot Net Framework ) 
+
+dotnet new mvc -au None -o rohitoSharp ( Create Web app called rohitoSharp )
