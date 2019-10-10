04d560e05c98b30d823b139e1b72be7920bf591b
Rohit Bhambhani <rohito@Rohitos-MacBook-Pro-Work.local>
2019-10-09 23:06:40 -0400

    Generate Random Number To Be Guessed

--- Program.cs
+++ Program.cs
@@ -32,92 +32,102 @@ namespace CSharp
             Console.WriteLine("Let's Play a game, {0} ", inputName);
 
 
-            // Set Correct Number To Be Guessed By User
+            while (true) {
+                // Set Correct Number To Be Guessed By User
 
-            int correctNumber = 12;
+                // int correctNumber = 12;
 
-            // Initialize guess variable 
-            int guess = 0;
+                // Create a new Random Object
 
+                Random random = new Random();
 
-            // Ask user for number 
-            Console.WriteLine("Guess a number between 1 and 15");
-
-            // While Loop Untill The User Guesses the Correct Number 
-            while (guess != correctNumber)
-            {
-                string input = Console.ReadLine();
+                int correctNumber = random.Next(1, 15);
 
 
-                // Check if input is in the correct format 
-
-                 if ( !int.TryParse(input, out guess) )
-                { 
-
-                    // Change Text Color
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-
-                    // Write out App Variales 
-                    Console.WriteLine( input + " !! Really! " + inputName + ". Enter an actual number. You dummy!!!");
-
-                    // Reset Color 
-                    Console.ResetColor();
-
-                    // Keep Going
-                    continue;
-                }
+                // Initialize guess variable 
+                int guess = 0;
 
 
+                // Ask user for number 
+                Console.WriteLine("Guess a number between 1 and 15");
 
-
-
-
-                // Cast to int and make as value for guess variable 
-
-                guess = Int32.Parse(input);
-
-
-                // Match guess to correctNumber 
-
-                if (guess <= 0 || guess >= 16 )
+                // While Loop Untill The User Guesses the Correct Number 
+                while (guess != correctNumber)
                 {
+                    string input = Console.ReadLine();
 
-                    // Change Text Color
-                    Console.ForegroundColor = ConsoleColor.Blue;
 
-                    // Write out App Variales 
-                    Console.WriteLine(guess + " is the not between 1 and 15 , " + inputName + ". You dummy!!!");
+                    // Check if input is in the correct format 
 
-                    // Reset Color 
-                    Console.ResetColor();
-                }
+                    if (!int.TryParse(input, out guess))
+                    {
 
-                else if (guess != correctNumber)
-                {
+                        // Change Text Color
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
 
-                    // Change Text Color
-                    Console.ForegroundColor = ConsoleColor.Red;
+                        // Write out App Variales 
+                        Console.WriteLine(input + " !! Really! " + inputName + ". Enter an actual number. You dummy!!!");
 
-                    // Write out App Variales 
-                    Console.WriteLine(guess + " is the wrong Number, " + inputName);
+                        // Reset Color 
+                        Console.ResetColor();
 
-                    // Reset Color 
-                    Console.ResetColor();
+                        // Keep Going
+                        continue;
+                    }
 
-                }
-                else
-                {
-                    // Change Text Color
-                    Console.ForegroundColor = ConsoleColor.Green;
+                    // Cast to int and make as value for guess variable 
 
-                    // Write out App Variales 
-                    Console.WriteLine(guess + " is the CORRECT Number, " + inputName);
+                    guess = Int32.Parse(input);
 
-                    // Reset Color 
-                    Console.ResetColor();
+                    // Match guess to correctNumber 
+
+                    if (guess <= 0 || guess >= 16)
+                    {
+
+                        // Change Text Color
+                        Console.ForegroundColor = ConsoleColor.Blue;
+
+                        // Write out App Variales 
+                        Console.WriteLine(guess + " is the not between 1 and 15 , " + inputName + ". You dummy!!!");
+
+                        // Reset Color 
+                        Console.ResetColor();
+                    }
+
+                    else if (guess != correctNumber)
+                    {
+
+                        // Change Text Color
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        // Write out App Variales 
+                        Console.WriteLine(guess + " is the wrong Number, " + inputName);
+
+                        // Reset Color 
+                        Console.ResetColor();
+
+                    }
+                    else
+                    {
+                        // Change Text Color
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        // Write out App Variales 
+                        Console.WriteLine(guess + " is the CORRECT Number, Well Done " + inputName);
+
+                        // Reset Color 
+                        Console.ResetColor();
+
+                    }
+
+                                
+
+
                 }
 
             }
+
+
         }
     }
 }
