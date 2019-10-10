2ed2316871bc92c244e37e544554ea8ace93677f
Rohit Bhambhani <rohito@Rohitos-MacBook-Pro-Work.local>
2019-10-09 21:57:27 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name  = "Rohito Bhambhani";
+            Console.WriteLine("Hello World! Welcome, Mr. " + name );
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+csc Program.cs ( Compile Program )
+
+mono Program.exe ( Cross Platform Dot Net Framework ) 
